bb9bd12c6fbec86a60bfa3b26e54ba685e411b1c
adammharris <97053407+adammharris@users.noreply.github.com>
2025-10-16 22:15:35 -0600

    feat: replace image banner with MDX Hero component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     responsiveStyles: true,
     layout: "constrained",
   },
+
+  integrations: [mdx()],
 });
